3065776730a4dc8233685aa08630880b6e0793bb
KAUSHIK BAIDYA <47146123+KaushikBaidya@users.noreply.github.com>
2022-09-28 11:38:06 +0600

    as

--- dbServer.js
+++ dbServer.js
@@ -3,11 +3,11 @@ import { createPool } from "mysql";
 import * as dotenv from "dotenv";
 dotenv.config();
 
-const app = express();
 const DB_HOST = process.env.DB_HOST;
 const DB_USER = process.env.DB_USER;
 const DB_DATABASE = process.env.DB_DATABASE;
 const DB_PORT = process.env.DB_PORT;
+const port = process.env.PORT;
 
 const db = createPool({
   connectionLimit: 100,
@@ -17,6 +17,8 @@ const db = createPool({
   database: DB_DATABASE,
   port: DB_PORT,
 });
+const app = express();
+app.listen(port, () => console.log(`Server Started on port ${port}...`));
 
 db.getConnection((err, connection) => {
   if (err) throw err;
